4fba5aae6c79108618fa50e69415ab8df9cd936c
oscar catari <alu0100825893@ull.edu.es>
2017-05-07 22:38:15 +0000

    prueba fallida arreglada

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -3,13 +3,17 @@ var expect = chai.expect
 
 describe('Arbol', function(){
     
-    it ('Prueba', function () {
-        expect(PEG.parse("a = b+2")).to.deep.equal({ type: '=',
-                                                      left: { type: 'ID', value: 'a' },
-                                                      right: 
-                                                       { type: '+',
-                                                         left: { type: 'ID', value: 'b' },
-                                                         right: { type: 'NUMBER', value: '2' } } });
+    it ('Se pueden hacer asignaciones', function () {
+        expect(PEG.parse("a = b + 2.")).to.deep.equal({ program: 
+                                                       { declarations: { constants: null, variables: null },
+                                                         functions: null,
+                                                         statements: 
+                                                          { type: '=',
+                                                            left: { type: 'ID', value: 'a' },
+                                                            right: 
+                                                             { type: '+',
+                                                               left: { type: 'ID', value: 'b' },
+                                                               right: { type: 'NUMBER', value: '2' } } } } });
     });
     
 });
